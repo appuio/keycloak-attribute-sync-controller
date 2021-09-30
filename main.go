7e7bbb324945570a3774e2da54f3d76aeb9561f1
Sebastian Widmer <sebastian.widmer@vshn.net>
2021-09-30 15:13:27 +0200

    Add openshift users/v1 to scheme

--- main.go
+++ main.go
@@ -24,6 +24,7 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
+	userv1 "github.com/openshift/api/user/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
@@ -47,6 +48,8 @@ func init() {
 
 	utilruntime.Must(keycloakv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
+
+	utilruntime.Must(userv1.AddToScheme(scheme))
 }
 
 func main() {
